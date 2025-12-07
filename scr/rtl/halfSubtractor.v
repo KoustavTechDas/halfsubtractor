93010ea8eb2971b17d8d8919b72195198579f5f1
KoustavTechDas <koustav20013@gmail.com>
2025-12-07 13:34:39 +0530

    uploading the file structure and the design and testbench code

---
+++ scr/rtl/halfSubtractor.v
@@ -0,0 +1,8 @@
+module halfSubtractor(
+  input a,b,
+  output d, br
+);
+  assign d = a ^ b;
+  assign br = (~a) & b;
+endmodule
+
---
+++ sim/testbench/halfSubtractor_tb.v
@@ -0,0 +1,18 @@
+module halfSubtractor_tb;
+  reg a,b;
+  wire d,br;
+  
+halfSubtractor HA(a,b,d,br);
+  
+  initial begin
+    $monitor("Time=%0t, A=%b, B=%b, diff=%b, borrow=%b",$time,a,b,d,br);
+  end
+  
+  initial begin
+    a=0; b=0; #5;
+    a=0; b=1; #5;
+    a=1; b=0; #5;
+    a=1; b=1; #5;
+    $stop;
+  end
+endmodule
