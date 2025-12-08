86bfe88618aded48412bb35eca4ec268e1d7c9f0
KoustavTechDas <koustav20013@gmail.com>
2025-12-08 18:52:20 +0530

    adding the images

--- sim/testbench/halfSubtractor_tb.v
+++ sim/testbench/halfSubtractor_tb.v
@@ -2,7 +2,7 @@ module halfSubtractor_tb;
   reg a,b;
   wire d,br;
   
-halfSubtractor HA(a,b,d,br);
+halfSubtractor HS(a,b,d,br);
   
   initial begin
     $monitor("Time=%0t, A=%b, B=%b, diff=%b, borrow=%b",$time,a,b,d,br);
